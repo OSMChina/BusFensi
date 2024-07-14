676b3468aed7dbd7bc83fe7fe7ee5ebc9cf72300
fltb <fltbk@outlook.com>
2024-07-14 17:06:20 +0800

    feat: textures for pixi

---
+++ src/pixi/textures/index.js
@@ -0,0 +1,5 @@
+import { Assets } from "pixi.js";
+
+
+export const locationPinTexture = await Assets.load('src/location-pin.svg');
+export const circleTexture = await Assets.load('src/circle.svg');
