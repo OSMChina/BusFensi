ff44f8cfd158b234d01c2f2765f2f0296a86458e
fltb <fltbk@outlook.com>
2024-08-05 15:23:30 +0800

    feat: update map., action merge, and some fix

--- src/logic/actions/PIXIViewpointMove.js
+++ src/logic/actions/PIXIViewpointMove.js
@@ -1,39 +1,56 @@
-import { deepMerge, deepReverseDiff } from "../../utils/helper/object";
+import { getWGS84LocateByPixel } from "../../utils/geo/mapProjection";
+import { deepCopy, deepMerge } from "../../utils/helper/object";
 import { AbstractAction } from "./AbstractAction";
-import { actionList } from "./ActionList";
-import { loadBBox, OSMLoadDataAction } from "./OSMLoadedDataAction";
 
 export class PIXIViewpointMove extends AbstractAction {
-    constructor(location, layer) {
+    /**
+     * 
+     * @param {Number} deltax
+     * @param {Number} deltay 
+     * @param {import('../states/type').Layers} layers
+     * @param {import('pixi.js').Application} scene  
+     */
+    constructor(deltax, deltay, layers, scene) {
         super()
-        this.location = location
-        this.layer = layer
+        /** @type {Number} */
+        this.deltax = deltax
+        /** @type {Number} */
+        this.deltay = deltay
+        /** @type {import('../states/type').Layers}*/
+        this.layers = layers
+        /** @type {import('pixi.js').Application} */
+        this.scene = scene
+
         this.type = 'pixi-viewpoint-move'
     }
     do(state) {
+        const { viewpoint, zoom } = state.view;
+        const pixpoint = {
+            x: this.deltax + this.scene.canvas.width / 2,
+            y: this.deltay + this.scene.canvas.height / 2
+        }
+        const location = getWGS84LocateByPixel(pixpoint, viewpoint, zoom, this.scene.canvas.width, this.scene.canvas.height);
         this._redo = {
             view: {
-                viewpoint: this.location
+                viewpoint: location
             }
         }
-        this._undo = deepReverseDiff(state, this._redo)
+        this._undo = {
+            view: {
+                viewpoint: deepCopy(state.view.viewpoint)
+            }
+        }
         this._state = state;
-        deepMerge(this._state, this._redo);
+        this._state.view.viewpoint = deepCopy(location);
         // then request data
-        loadBBox(this._state)
-            .then(bboxObj => {
-                if (bboxObj.osm.bounds) {
-                    const osmLoadedDataAction = new OSMLoadDataAction(this.layer, bboxObj);
-                    actionList.do(osmLoadedDataAction);
-                }
-            })
+        this.updateMap()
     }
-    redo(){
+    redo() {
         deepMerge(this._state, this._redo)
         // then call layer to update point
         this.updateMap()
     }
-    undo(){
+    undo() {
         deepMerge(this._state, this._undo)
         // then call layer to update point
         this.updateMap()
@@ -41,20 +58,34 @@ export class PIXIViewpointMove extends AbstractAction {
 
     mergeAction(action, state) {
         if (action.type === this.type) {
+            const { zoom } = state.view
+            const { viewpoint } = action._undo.view // original viewpoint
+            const pixpoint = {
+                x: this.deltax + this.scene.canvas.width / 2,
+                y: this.deltay + this.scene.canvas.height / 2
+            }
+            const location = getWGS84LocateByPixel(pixpoint, viewpoint, zoom, this.scene.canvas.width, this.scene.canvas.height);
+            console.log("from", viewpoint, "to", location)
+
             this._redo = {
                 view: {
-                    viewpoint: this.location
+                    viewpoint: location
                 }
             }
             this._undo = action._undo;
             this._state = state;
-            deepMerge(this._state, this._redo);
+            this._state.view.viewpoint = deepCopy(location);
+            console.log('merged action')
             this.updateMap()
-            return true
+            return true;
         }
         return false
     }
     updateMap() {
-        // 等以后实现
+        for (const key in this.layers) {
+            if (Object.prototype.hasOwnProperty.call(this.layers, key)) {
+                this.layers[key].updateViewpoint(this._state.view.viewpoint)
+            }
+        }
     }
 }
