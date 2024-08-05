245f9ca0d7461e7f86c35c0c0625a61047ea9773
fltb <fltbk@outlook.com>
2024-08-05 15:24:00 +0800

    feat: zoom map

--- src/logic/actions/ZoomAction.js
+++ src/logic/actions/ZoomAction.js
@@ -8,18 +8,24 @@ export class ZoomAction extends AbstractAction {
     /**
      * 
      * @param {Boolean} axis 
+     * @param {import('../states/type').Layers} layers
+     * @param {import('pixi.js').Application} scene  
      */
-    constructor(axis) {
+    constructor(axis, layers, scene) {
         super();
         this.axis = axis;
         this.type = 'zoom'
+        this.layers = layers
+        this.scene = scene
     }
     do(state) {
-        const zoom = this.state.view.zoom + this.axis ? 1 : -1;
+        this._state = state
+        const zoom = this._state.view.zoom + (this.axis ? 1 : -1);
+        console.log(`zoom from ${this.state.view.zoom} to ${zoom}`)
         if (zoom > MAXZOOM) {
             this._redo = {}
             this._undo = {}
-            this.state = state
+            this._state = state
             return
         }
         this._redo = {
@@ -29,10 +35,10 @@ export class ZoomAction extends AbstractAction {
         }
         this._undo = deepReverseDiff(state, this._redo)
         deepMerge(this._state, this._redo);
-        loadBBox(this._state)
+        loadBBox(this._state, this._state.view.viewpoint, this._state.view.zoom, this.scene.canvas.width, this.scene.canvas.height)
             .then(bboxObj => {
-                if (bboxObj.osm.bounds) {
-                    const osmLoadedDataAction = new OSMLoadDataAction(this.layer, bboxObj);
+                if (bboxObj?.osm?.bounds) {
+                    const osmLoadedDataAction = new OSMLoadDataAction(this.layers, bboxObj);
                     actionList.do(osmLoadedDataAction);
                 }
             })
@@ -47,6 +53,10 @@ export class ZoomAction extends AbstractAction {
         this.updateMap()
     }
     updateMap() {
-        // 等以后实现
+        for (const key in this.layers) {
+            if (Object.prototype.hasOwnProperty.call(this.layers, key)) {
+                this.layers[key].updateZoom(this._state.view.zoom)
+            }
+        }
     }
 }
