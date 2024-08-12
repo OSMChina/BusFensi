3eece98746d219506976e1be62794299530ade0f
fltb <fltbk@outlook.com>
2024-08-12 22:49:30 +0800

    feat: new state machine for react pixi

---
+++ src/logic/states/stateMachine.ts
@@ -0,0 +1,61 @@
+import { defaultState } from './stateGraph';
+import { State, Bucket } from './type';
+import { FederatedEvent } from 'pixi.js';
+
+class StateMachine {
+    current: State;
+    targetId: string | null;
+    bucket: Bucket;
+    shiftKey: boolean
+    constructor() {
+        this.current = defaultState;
+        this.targetId = null;
+        this.bucket = {
+            mapDrag: null,
+            componentTargetId: null,
+            componentDrag: null
+        };
+        this.shiftKey = false
+        document.addEventListener('keydown', (e) => {
+            this.shiftKey = e.shiftKey
+        })
+        document.addEventListener('keyup', (e)=> [
+            this.shiftKey = e.shiftKey
+        ])
+    }
+
+    /**
+     * Hook PIXI component
+     */
+    hookPIXIComponent(event: FederatedEvent, targetId: string) {
+        this.targetId = targetId;
+        this.transform(event);
+    }
+
+    /**
+     * Hook PIXI scene
+     * 
+     */
+    hookPIXIScene(event: unknown) {
+        this.transform(event);
+    }
+
+    /**
+     * Transform state based on the event
+     * 
+     */
+    transform(event: unknown) {
+        for (let i = 0; i < this.current.nxt.length; i++) {
+            const { state, transfer } = this.current.nxt[i];
+            if (transfer(event)) {
+                this.current = state;
+                return;
+            }
+        }
+        if (!this.current.retain(event)) {
+            this.current = defaultState;
+        }
+    }
+}
+
+export const stateMachine = new StateMachine();
--- src/logic/states/type.d.ts
+++ src/logic/states/type.d.ts
@@ -1,16 +1,14 @@
-import { AbstractComponent } from "../../pixi/components/AbstructComponent"
-import { BackgroundLayer } from "../../pixi/layers/BackgroundLayer"
-import { EditableLayer } from "../../pixi/layers/EditableLayer"
+import { PointWGS84 } from "../../utils/geo/types"
 
 export interface State {
     /**
      * Name of state
      */
-    type: String,
+    type: string,
     /**
      * 判断是否停留在当前状态的时候会跑一遍这个函数，可能会处理一些逻辑
      */
-    retain: (event: Event) => Boolean,
+    retain: (event) => boolean,
     /**
      * posibble state to transfer 
      */
@@ -20,23 +18,19 @@ export interface State {
          * 是否可以转移到此状态。同时也会处理一些其他变更，比如生成 Action
          * 会保证仅仅由前趋结点 nxt 记录中 transfer 而来
          */
-        transfer: (event: Event) => Boolean,
+        transfer: (event) => boolean,
     }>
 }
 
-export interface Layers {
-    backgroundLayer: BackgroundLayer
-    editableLayer: EditableLayer
-}
-
 export interface Bucket {
-    mapDrag: {
+    mapDrag?: {
+        x: number
+        y: number,
+        viewpointBeforeDrag: PointWGS84
+    } | null
+    componentTargetId?: string | null
+    componentDrag?: {
         x: number
         y: number
-    }
-    componentTarget: AbstractComponent
-    componentDrag: {
-        x: number
-        y: number
-    }
+    } | null
 }
