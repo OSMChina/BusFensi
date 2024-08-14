6fc45d22560831c8c5c6c8c002fd1a4473a8c7d6
fltb <fltbk@outlook.com>
2024-08-14 19:34:20 +0800

    feat: added tailwindcss daisyui nad fontawesome

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+import daisyui from "daisyui"
+
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    daisyui
+  ],
+};
