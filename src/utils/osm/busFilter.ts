3eedf27718c9ec5820ac0119204cc2c500a3df87
fltb <fltbk@outlook.com>
2024-09-01 16:48:06 +0800

    feat: filter of collection

--- src/utils/osm/busFilter.ts
+++ src/utils/osm/busFilter.ts
@@ -1,7 +1,7 @@
 // see https://wiki.openstreetmap.org/wiki/Public_transport for detail
 
 import { Tag } from "../../api/osm/type";
-import { NodesObj, RelationsObj, WaysObj } from "../../logic/model/type";
+import { CollectionItem, NodesObj, RelationsObj, WaysObj } from "../../logic/model/type";
 import { T2Arr } from "../helper/object";
 import { getPropFromTags } from "./getTag";
 
@@ -23,6 +23,64 @@ export function isBusStop(tags: Tag[]): boolean {
         && tags.some(tag => tag["@_k"] === "highway" && tag["@_v"] === "bus_stop")
 }
 
+const getCollector = (
+    filteredNodes: Set<string>,
+    filteredWays: Set<string>,
+    filteredRelations: Set<string>,
+    nodes: NodesObj,
+    ways: WaysObj,
+    relations: RelationsObj
+) => {
+    const collectMembers = (id: string, type: "node" | "way" | "relation") => {
+        if (type === "relation") {
+            const relation = relations[id];
+            if (!relation) return;
+
+            if (!filteredRelations.has(id)) {
+                filteredRelations.add(id)
+            }
+            T2Arr(relation.member).forEach((member) => {
+                const key = member["@_ref"],
+                    type = member["@_type"]
+                if (type === 'node') {
+                    if (!filteredNodes.has(key) && nodes[key]) {
+                        filteredNodes.add(key)
+                    }
+                } else if (type === 'way') {
+                    if (!filteredWays.has(key) && ways[key]) {
+                        collectMembers(key, "way");
+                    }
+                } else if (type === 'relation') {
+                    if (!filteredRelations.has(key) && relations[key]) {
+                        collectMembers(key, "relation");
+                    }
+                }
+            });
+        } else if (type === "way") {
+            const way = ways[id]
+            if (!way) return;
+
+            if (!filteredWays.has(id)) {
+                filteredWays.add(id)
+            }
+            T2Arr(way.nd).forEach(nd => {
+                const key = nd["@_ref"]
+                if (!filteredNodes.has(key) && nodes[key]) {
+                    filteredNodes.add(key)
+                }
+            })
+        } else if (type === "node") {
+            const node = nodes[id]
+            if (!node) return;
+
+            if (!filteredNodes.has(id)) {
+                filteredNodes.add(id)
+            }
+        }
+    };
+    return collectMembers
+}
+
 /**
  * Filters and collects nodes, ways, and relations based on the bus PTv2 schema.
  * 
@@ -35,61 +93,84 @@ export function filterBusPTv2(
     nodes: NodesObj,
     ways: WaysObj,
     relations: RelationsObj
-): {
-    nodes: NodesObj,
-    ways: WaysObj,
-    relations: RelationsObj
-} {
-    const filteredNodes: NodesObj = {};
-    const filteredWays: WaysObj = {};
-    const filteredRelations: RelationsObj = {};
+): CollectionItem {
+    const filteredNodes = new Set<string>()
+    const filteredWays = new Set<string>()
+    const filteredRelations = new Set<string>()
 
-    // Helper function to collect all members of a relation
-    const collectMembers = (relationId: string) => {
-        const relation = relations[relationId];
-        if (!relation) return;
-
-        T2Arr(relation.member).forEach((member) => {
-            if (member["@_type"] === 'node') {
-                if (!filteredNodes[member["@_ref"]]) {
-                    filteredNodes[member["@_ref"]] = nodes[member["@_ref"]];
-                }
-            } else if (member["@_type"] === 'way') {
-                if (!filteredWays[member["@_ref"]]) {
-                    filteredWays[member["@_ref"]] = ways[member["@_ref"]];
-                }
-            } else if (member["@_type"] === 'relation') {
-                if (!filteredRelations[member["@_ref"]]) {
-                    filteredRelations[member["@_ref"]] = relations[member["@_ref"]];
-                    // Recurse into nested relations
-                    collectMembers(String(member["@_ref"]));
-                }
-            }
-        });
-    };
+    // Helper function to collect all members of a feature
+    const collectMembers = getCollector(filteredNodes, filteredWays, filteredRelations, nodes, ways, relations)
 
     // Process route master and route relations
     Object.values(relations).forEach((relation) => {
         if (getPropFromTags("type", relation.tag) === 'route_master' && getPropFromTags('route_master', relation.tag) === 'bus') {
             // Collect members of the route master relation
-            collectMembers(String(relation["@_id"]));
+            collectMembers(relation["@_id"], "relation");
         } else if (getPropFromTags("type", relation.tag) === 'route' && getPropFromTags('public_transport:version', relation.tag) === '2') {
             // Collect members of the route relation
-            collectMembers(String(relation["@_id"]));
+            collectMembers(relation["@_id"], "relation");
         }
     });
 
     // Optionally handle roundtrip routes
     Object.values(relations).forEach((relation) => {
-        if (getPropFromTags('roundtrip', relation.tag) === 'yes') {
+        if (getPropFromTags("type", relation.tag) === 'route' && getPropFromTags('public_transport:version', relation.tag) === '2' && getPropFromTags('roundtrip', relation.tag) === 'yes') {
             // Collect members of roundtrip relations
-            collectMembers(String(relation["@_id"]));
+            collectMembers(relation["@_id"], "relation");
         }
     });
 
     return {
-        nodes: filteredNodes,
-        ways: filteredWays,
-        relations: filteredRelations
+        nodesId: filteredNodes,
+        waysId: filteredWays,
+        relationsId: filteredRelations
     };
 }
+
+/**
+ * Filters and collects nodes, ways, and relations in highway system.
+ * 
+ * @param nodes - Object containing nodes data.
+ * @param ways - Object containing ways data.
+ * @param relations - Object containing relations data.
+ * @returns An object containing filtered nodes, ways, and relations.
+ */
+
+export function filterHighway(
+    nodes: NodesObj,
+    ways: WaysObj,
+    relations: RelationsObj
+): CollectionItem {
+    const filteredNodes = new Set<string>()
+    const filteredWays = new Set<string>()
+    const filteredRelations = new Set<string>()
+
+    // Helper function to collect all members of a feature
+    const collectMembers = getCollector(filteredNodes, filteredWays, filteredRelations, nodes, ways, relations)
+
+    // Process highway relation
+    Object.values(relations).forEach((relation) => {
+        if (getPropFromTags("highway", relation.tag)) {
+            collectMembers(relation["@_id"], "relation");
+        }
+    });
+
+    Object.values(ways).forEach((way) => {
+        if (getPropFromTags("highway", way.tag)) {
+            collectMembers(way["@_id"], "way");
+        }
+    });
+
+    Object.values(nodes).forEach((node) => {
+        if (getPropFromTags("highway", node.tag)) {
+            collectMembers(node["@_id"], "node");
+        }
+    });
+
+    return {
+        nodesId: filteredNodes,
+        waysId: filteredWays,
+        relationsId: filteredRelations
+    };
+}
+
