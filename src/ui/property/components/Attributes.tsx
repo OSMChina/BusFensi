a5719922988f0a49acfec92284668928cf06de90
fltb <fltbk@outlook.com>
2024-09-09 18:05:52 +0800

    fix: display attributes as menu-xs

--- src/ui/property/components/Attributes.tsx
+++ src/ui/property/components/Attributes.tsx
@@ -3,16 +3,18 @@ import { Node, Way, Relation } from "../../../api/osm/type";
 
 function Attributes({ meta }: { meta: Node | Way | Relation }) {
     return (
-        <ul className="space-y-2">
+        <ul className="menu menu-xs bg-200 rounded-box">
             {Object.keys(meta)
                 .filter(
                     (key) =>
                         Object.prototype.hasOwnProperty.call(meta, key) && key.startsWith("@_")
                 )
                 .map((key) => (
-                    <li key={key} className="flex justify-between p-2 bg-base-200 rounded-md shadow-sm">
-                        <span className="font-semibold">{key.substring(2)}</span>
-                        <span className="ml-2 text-base-content">{(meta as any)[key]}</span>
+                    <li key={key}>
+                        <span>
+                            <span className="font-semibold">{key.substring(2)}</span>
+                            <span className="ml-auto text-base-content">{(meta as any)[key]}</span>
+                        </span>
                     </li>
                 ))}
         </ul>
