554e73982129e22f61077d0d03a74784bcb74cc6
fltb <fltbk@outlook.com>
2024-09-09 18:12:52 +0800

    fix: rewrite property view

--- src/ui/property/index.tsx
+++ src/ui/property/index.tsx
@@ -2,32 +2,39 @@ import useBearStoreWithUndo from "../../logic/model/store";
 import NodeProperty from "./views/NodeProperty";
 import WayProperty from "./views/WayProperty";
 import RelationProperty from "./views/RelationProperty";
+import { useEffect, useState } from "react";
+import { useShallow } from "zustand/react/shallow";
+import { cn } from "../../utils/helper/object";
 
 function PropertyView() {
-  const [activeId, type] = useBearStoreWithUndo(state => {
-    const id = state.selectedComponent[0]
-    if (id !== undefined) {
-      return [id, state.renderedOSMFeatureMeta.id2type[id]]
-    } else {
-      return [undefined, undefined]
+  const selected = useBearStoreWithUndo(useShallow(state => state.selectedComponent))
+  const id2type = useBearStoreWithUndo(useShallow(state => state.renderedOSMFeatureMeta.id2type))
+  const [activeId, setActiveId] = useState<string | undefined>(selected[0])
+  useEffect(() => {
+    if (!activeId && selected.length > 0) {
+      setActiveId(selected[0])
+    } else if (activeId && !selected.includes(activeId)) {
+      setActiveId(undefined)
     }
-  });
+  }, [selected, activeId, setActiveId])
 
-  let child;
-  if (typeof activeId === "string") {
-    if (type === "node") {
-      child = <NodeProperty id={activeId} />
-    } else if (type === "way") {
-      child = <WayProperty id={activeId} />
-    } else {
-      child = <RelationProperty id={activeId} />
+  return <div className="property-view min-h-1/2 h-1/2 max-h-1/2 w-full flex flex-col p-1 rounded bg-base-100">
+    <div role="tablist" className="tabs tabs-lifted tabs-xs">
+      {selected.map(id => (
+        <a key={id} role="tab" className={cn("tab", id === activeId && "tab-active")} onClick={() => setActiveId(id)}>{id}</a>
+      ))}
+    </div>
+    {activeId ?
+      (id2type[activeId] === "node" ?
+        <NodeProperty id={activeId} />
+        : id2type[activeId] === "way" ?
+          <WayProperty id={activeId} />
+          : id2type[activeId] === "relation" ?
+            <RelationProperty id={activeId} />
+            : <div>Unknown type for id {activeId}</div>
+      )
+      : <div>Please select some component to show prop edit view </div>
     }
-  } else {
-    child = <span>{"please select some components to edit"}</span>
-  }
-
-  return <div className="property-view min-h-1/2 h-1/2 max-h-1/2 w-full max-w-full flex flex-col p-1 rounded bg-base-100 overflow-x-scroll overflow-y-scroll">
-    {child}
   </div>
 }
 
