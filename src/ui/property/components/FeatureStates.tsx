a72d206a8b2f6d61d3b3a60f7afea1f242e2a5e9
fltb <fltbk@outlook.com>
2024-09-09 18:06:12 +0800

    fix: featureState in badge-xs

--- src/ui/property/components/FeatureStates.tsx
+++ src/ui/property/components/FeatureStates.tsx
@@ -3,11 +3,11 @@ import useBearStoreWithUndo from "../../../logic/model/store"
 
 function FeatureState({ id }: { id: string }) {
     const status = useBearStoreWithUndo(useShallow((state) => state.renderedFeatureState[id]))
-    return <div className="flex flex-wrap gap-2 mb-4">
+    return <div className="flex flex-wrap gap-1">
         {Object.entries(status)
             .filter(([, value]) => value)
             .map(([key]) => (
-                <span key={key} className="badge badge-info">
+                <span key={key} className="badge badge-info badge-xs">
                     {key}
                 </span>
             ))}
