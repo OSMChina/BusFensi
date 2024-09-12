7eaf857a81343b14afe85eb8af3c65644c40abc6
fltb <fltbk@outlook.com>
2024-09-12 10:44:00 +0800

    feat: allow edit for member list item

--- src/ui/property/components/MemberListItem.tsx
+++ src/ui/property/components/MemberListItem.tsx
@@ -7,14 +7,20 @@ import { T2Arr } from "../../../utils/helper/object"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCheck, faDeleteLeft, faDownload } from "@fortawesome/free-solid-svg-icons"
 import { ItemRefObj } from "../../../logic/model/type"
+import { faPenToSquare } from "@fortawesome/free-regular-svg-icons"
 
-function MemberListItem({ id, type, onDel, select }: {
+function MemberListItem({ id, type, onDel, select, edit }: {
     id: string,
     type: "node" | "way" | "relation",
     onDel: (item: ItemRefObj) => void,
     select?: {
         active?: ItemRefObj
         setter: (item: ItemRefObj) => void
+    },
+    edit?: {
+        text: string,
+        setter: (text: string) => void
+        onBlur: () => void
     }
 }) {
     const loaded = useBearStoreWithUndo(useShallow((state) => state.collections.global[`${type}sId`].has(id)))
@@ -22,6 +28,7 @@ function MemberListItem({ id, type, onDel, select }: {
     const addWayAction = useBearStoreWithUndo((state) => state.addWayAction)
     const addRelationAction = useBearStoreWithUndo((state) => state.addRelationAction)
     const [loading, setLoading] = useState(false)
+    const [editing, setEditing] = useState(false)
 
     const loadElement = async (
         id: string) => {
@@ -43,6 +50,29 @@ function MemberListItem({ id, type, onDel, select }: {
     return <div className={`rounded-sm border text-xs flex flex-row pl-1 ${(select?.active?.id === id && select.active.type) ? "bg-neutral text-neutral-content" : "bg-base-200 text-base-content"}`}>
         <span>{`${type}-${id}`}</span>
         <span className="ml-auto"></span>
+        {edit && editing ?
+            <input
+                className="input input-xs input-bordered"
+                type="text"
+                value={edit.text}
+                onMouseDown={e => e.stopPropagation()}
+                onKeyDown={e => e.stopPropagation()}
+                onChange={(e) => edit.setter(e.target.value)}
+                onBlur={() => {
+                    edit.onBlur()
+                    setEditing(false)
+                }}
+            />
+            :
+            <button className="btn btn-square btn-xs"
+                onMouseDown={(event) => {
+                    event.stopPropagation();
+                    setEditing(true)
+                }}
+            >
+                <FontAwesomeIcon icon={faPenToSquare} />
+            </button>
+        }
         {!loaded && (loading ?
             (<span className="loading loading-spinner loading-xs"></span>)
             : (
@@ -54,6 +84,7 @@ function MemberListItem({ id, type, onDel, select }: {
                 </button>
             ))
         }
+
         {select && (<button className="btn btn-square btn-accent btn-xs" onMouseDown={(event) => {
             event.stopPropagation();
             select.setter({ id: id, type: type })
