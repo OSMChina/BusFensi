c46fba3c00794704c077061dcecb3ee4ccb9567f
fltb <fltbk@outlook.com>
2024-09-12 10:46:02 +0800

    feat: added add node

--- src/ui/property/views/WayProperty.tsx
+++ src/ui/property/views/WayProperty.tsx
@@ -27,6 +27,8 @@ import MemberListItem from "../components/MemberListItem";
 import { useState } from "react";
 import Draggable from "../components/Dragable";
 import { ItemRefObj } from "../../../logic/model/type";
+import InsertMember from "./InsertMember";
+import { InsertHandeler } from "../type";
 
 function WayProperty({ id }: { id: string }) {
     const meta = useBearStoreWithUndo(useShallow((state) => state.renderedOSMFeatureMeta.ways[id]));
@@ -45,6 +47,10 @@ function WayProperty({ id }: { id: string }) {
     }
 
     const [{ activeId, activeType }, setActive] = useState<ActiveObj>({ activeId: undefined, activeType: undefined });
+    const [localActiveNd, setLocalActive] = useState<ItemRefObj | undefined>(undefined)
+    if (!meta) {
+        return null
+    }
     const items = T2Arr(meta.nd).map((nd) => ({ id: nd["@_ref"], nd: nd })); // only have node type so id is always unique
 
     function handleDragStart(event: DragStartEvent) {
@@ -77,32 +83,79 @@ function WayProperty({ id }: { id: string }) {
         })
     }
 
+    const itemsToNd = (items: {
+        id: string,
+        type: "way" | "node" | "relation"
+    }[]) => items.filter(item => !T2Arr(meta.nd).some(m => item.id === m["@_ref"])).map(item => ({ '@_ref': item.id }))
+
+    const handleInsertTop: InsertHandeler = (items) => {
+        console.log("Insert at Top: ", items);
+        modifyWayNoCommit(id, {
+            nd: [...itemsToNd(items), ...T2Arr(meta.nd)]
+        })
+        commitAction()
+    };
+
+    const handleInsertBottom: InsertHandeler = (items) => {
+        console.log("Insert at Bottom: ", items);
+        modifyWayNoCommit(id, {
+            nd: [...T2Arr(meta.nd), ...itemsToNd(items)]
+        })
+        commitAction()
+    };
+
+    const handleInsertAtActive: InsertHandeler = (items) => {
+        console.log("Insert at Active: ", items);
+        if (!localActiveNd) {
+            handleInsertBottom(items)
+        } else {
+            const ndArray = [...T2Arr(meta.nd)];
+            const insertIndex = ndArray.findIndex(m => m["@_ref"] === localActiveNd.id);
+            ndArray.splice(insertIndex + 1, 0, ...itemsToNd(items));
+            modifyWayNoCommit(id, {
+                nd: ndArray
+            });
+            commitAction();
+        }
+    };
+
     return (
         <div className="p-2 overflow-scroll">
             <h3 className="text-base font-semibold mb-2">Way {meta["@_id"]}</h3>
             <FeatureState id={id} type="way" />
             <Attributes meta={meta} />
             <Tags tags={T2Arr(meta.tag)} setTags={(tags) => { const metaNew = deepCopy(meta); metaNew.tag = tags; modifyWayNoCommit(id, metaNew) }} commitChange={commitAction} />
-            <DndContext
-                sensors={sensors}
-                collisionDetection={closestCenter}
-                onDragStart={handleDragStart}
-                onDragEnd={handleDragEnd}
-            >
-                <SortableContext
-                    items={items}
-                    strategy={verticalListSortingStrategy}
-                >
-                    {items.map(({ id }) => (
-                        <Draggable id={id} key={id}>
-                            <MemberListItem id={id} onDel={handleDelete} type="node" />
-                        </Draggable>
-                    ))}
-                </SortableContext>
-                <DragOverlay>
-                    {(activeId && activeType) ? <MemberListItem id={activeId} type={activeType} onDel={() => { }} /> : null}
-                </DragOverlay>
-            </DndContext>
+            <div className="flex flex-row">
+                <div>
+                    <DndContext
+                        sensors={sensors}
+                        collisionDetection={closestCenter}
+                        onDragStart={handleDragStart}
+                        onDragEnd={handleDragEnd}
+                    >
+                        <SortableContext
+                            items={items}
+                            strategy={verticalListSortingStrategy}
+                        >
+                            {items.map(({ id }) => (
+                                <Draggable id={id} key={id}>
+                                    <MemberListItem id={id} onDel={handleDelete} type="node" select={{ active: localActiveNd, setter: setLocalActive }} />
+                                </Draggable>
+                            ))}
+                        </SortableContext>
+                        <DragOverlay>
+                            {(activeId && activeType) ? <MemberListItem id={activeId} type={activeType} onDel={() => { }} /> : null}
+                        </DragOverlay>
+                    </DndContext>
+                </div>
+                <div className="relative bottom-0 right-0">
+                    <InsertMember
+                        handelInsertTop={handleInsertTop}
+                        handelIntertBottom={handleInsertBottom}
+                        handelInsertAtActive={handleInsertAtActive}
+                    />
+                </div>
+            </div>
             <FeatureRelation id={id} type="way" />
         </div>
     );
