5f95ac9c18fb1e3786cc4c3d96317ebed2cdd20a
fltb <fltbk@outlook.com>
2025-01-31 00:07:52 +0800

    feat: settings store

--- src/store/settings/store.ts
+++ src/store/settings/store.ts
@@ -1,4 +1,22 @@
-import { SettingsAction } from "./action";
-import { SettingsState } from "./initialState";
+import { create } from "zustand";
+import { createSettingsActionSlice, SettingsAction } from "./action";
+import { initialState, SettingsState } from "./initialState";
+import { createJSONStorage, devtools, persist } from "zustand/middleware";
 
 export type SettingsStore = SettingsAction & SettingsState
+
+const storageOptions = {
+    name: 'settingStore',
+    storage: createJSONStorage<SettingsStore>(() => localStorage),
+}
+
+// Apply the store creation with the correct middleware order
+export const useLocalAndUrlStore = create<SettingsStore>()(
+    devtools(
+        persist((...parameters) => ({
+            ...initialState,
+            ...createSettingsActionSlice(...parameters),
+        }), storageOptions),  // devtools applied after persist
+        { name: 'Settings' },
+    ),
+);
