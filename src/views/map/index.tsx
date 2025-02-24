7580344e50f8c34c63e4fc587fbe8152e822d157
fltb <fltbk@outlook.com>
2025-02-24 22:36:54 +0800

    feat(view/map): allow switch to pt edit mode

--- src/views/map/index.tsx
+++ src/views/map/index.tsx
@@ -3,10 +3,11 @@ import CommonEditApp from "./mode/common";
 import { HeaderBar } from "./components/HeaderBar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMap } from "@fortawesome/free-solid-svg-icons/faMap";
-import { faRoute } from "@fortawesome/free-solid-svg-icons/faRoute";
 import { cn } from "../../utils/helper/object";
+import PTEditApp from "./mode/ptedit";
+import { faBus } from "@fortawesome/free-solid-svg-icons/faBus";
 export function MapView({ width, height }: { width: number; height: number }) {
-  const HEADERBAR_HEIGHT = 48;
+  const HEADERBAR_HEIGHT = 42;
   const tabs = useMemo(() => [
     {
       title: <FontAwesomeIcon icon={faMap} />,
@@ -14,9 +15,9 @@ export function MapView({ width, height }: { width: number; height: number }) {
       component: () => <CommonEditApp width={width} height={height - HEADERBAR_HEIGHT} />,
     },
     {
-      title: <FontAwesomeIcon icon={faRoute} />,
+      title: <FontAwesomeIcon icon={faBus} />,
       tooltip: "Public transport edit mode",
-      component: () => <div className="p-4">其他编辑内容</div>,
+      component: () => <PTEditApp width={width} height={height - HEADERBAR_HEIGHT} />,
     },
   ], [width, height]);
 
@@ -26,9 +27,8 @@ export function MapView({ width, height }: { width: number; height: number }) {
     <div style={{ width, height, position: "relative" }}>
       <HeaderBar height={HEADERBAR_HEIGHT} leftSlot={<>
         {tabs.map((tab, index) => (
-          <div className="tooltip tooltip-right" data-tip={tab.tooltip}
-            key={index} onClick={() => setActive(index)}>
-            <button className={cn("btn btn-ghost text-lg", active === index && "btn-active")}>{tab.title}</button>
+          <div className="tooltip tooltip-right ml-1" data-tip={tab.tooltip} key={index}>
+            <button onClick={() => setActive(index)} className={cn("btn btn-ghost btn-square btn-sm", active === index && "btn-active")}>{tab.title}</button>
           </div>
         ))}
       </>}
