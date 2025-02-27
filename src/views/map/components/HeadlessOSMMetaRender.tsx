f0e41f4bc4c27b88711ea5d6cd91bddecfbcda69
fltb <fltbk@outlook.com>
2025-02-27 19:23:33 +0800

    fix(view/map): headless render forwardRef

--- src/views/map/components/HeadlessOSMMetaRender.tsx
+++ src/views/map/components/HeadlessOSMMetaRender.tsx
@@ -1,9 +1,10 @@
-import { ReactNode, useCallback, useMemo } from "react";
+import { forwardRef, ReactNode, useCallback, useMemo } from "react";
 import { FeatureMetaGroup } from "../../../type/osm/refobj";
 import { MapViewStatus } from "../../../utils/geo/types";
 import { getBoundsByScene } from "../../../utils/geo/mapProjection";
 import { Node, Way } from "../../../type/osm/meta";
 import { Container } from "@pixi/react";
+import { Container as PIXIContainer } from "pixi.js";
 
 type HeadlessRenderProps = React.ComponentProps<typeof Container> & {
     view: MapViewStatus,
@@ -12,29 +13,32 @@ type HeadlessRenderProps = React.ComponentProps<typeof Container> & {
     wayRenderer: (way: Way, index: number) => ReactNode,
 }
 
-export function HeadlessMetaRender({
-    view: { width, height, viewpoint, zoom },
-    meta: { node, way },
-    pointRenderer,
-    wayRenderer,
-    ...props
-}: HeadlessRenderProps) {
-    const { left, bottom, right, top } = useMemo(
-        () => getBoundsByScene(viewpoint, zoom, width, height),
-        [viewpoint, zoom, width, height])
+const HeadlessMetaRender = forwardRef<PIXIContainer, HeadlessRenderProps>(
+    function HeadlessMetaRender({ view: { width, height, viewpoint, zoom }, meta: { node, way }, pointRenderer, wayRenderer, ...props }, ref) {
+        const { left, bottom, right, top } = useMemo(
+            () => getBoundsByScene(viewpoint, zoom, width, height),
+            [viewpoint, zoom, width, height]
+        );
 
-    const inBound = useCallback(({ '@_lon': lon, '@_lat': lat }: Node) => {
-        return left <= lon && lon <= right && bottom <= lat && lat <= top
-    }, [left, bottom, right, top])
+        const inBound = useCallback(({ '@_lon': lon, '@_lat': lat }: Node) => {
+            return left <= lon && lon <= right && bottom <= lat && lat <= top;
+        }, [left, bottom, right, top]);
 
-    if (zoom < 16) {
-        return null;
+        if (zoom < 16) {
+            return null;
+        }
+
+        return (
+            <Container ref={ref} {...props}>
+                {Object.values(way)
+                    .filter(w => w.nd.map(nd => node[nd["@_ref"]]).some(inBound))
+                    .map(wayRenderer)}
+                {Object.values(node)
+                    .filter(inBound)
+                    .map(pointRenderer)}
+            </Container>
+        );
     }
+);
 
-    return <Container {...props}>
-        {Object.values(way).filter(w => w.nd.map(nd => node[nd["@_ref"]]).some(inBound))
-            .map(wayRenderer)}
-        {Object.values(node).filter(inBound)
-            .map(pointRenderer)}
-    </Container>
-}
+export default HeadlessMetaRender
