ebb09f068e62b8e8d8ad3cf4c8dbd8951e45eb81
fltb <fltbk@outlook.com>
2025-03-04 14:46:43 +0800

    chore(type): props of outline collection

--- src/type/view/outline/type.d.ts
+++ src/type/view/outline/type.d.ts
@@ -1,3 +1,9 @@
+import { ComponentProps } from "react";
 import { Node, Relation, Way } from "../../type/osm/meta";
+import { FeatureList } from "../../../views/map/components/collection/base/list";
 
 export type FilterFunc = (meta: Node | Way | Relation, type: "node" | "way" | "relation") => boolean
+
+export type OutlineCollectionProps =  {
+    filter?: FilterFunc
+}
