8f61e966873cb19be06ee2153785bf64635d405d
fltb <fltbk@outlook.com>
2025-03-10 22:12:53 +0800

    fix(view/map): click on rightclick menu wont affect components on pixi map now

--- src/views/map/components/RightCLickMenu.tsx
+++ src/views/map/components/RightCLickMenu.tsx
@@ -1,13 +1,13 @@
 import { Children, isValidElement, ReactElement } from "react";
 import { RightClickMenuProps } from "../../../type/view/map";
 
-export function RightClickMenu({ x, y, open, children }: RightClickMenuProps &{
+export function RightClickMenu({ x, y, open, children }: RightClickMenuProps & {
     children: ReactElement | ReactElement[]
 }) {
     if (!open) {
         return null;
     }
-    return <ul className="absolute rounded-lg bg-base-100 menu shadow p-0" style={{ top: y, left: x }}>
+    return <ul className="absolute rounded-lg bg-base-100 menu shadow p-0" onMouseDown={e => e.stopPropagation()} style={{ top: y, left: x }}>
         {Children.map(children, (child, index) => {
             if (!isValidElement(child)) { return null; }
             if (child.type === 'a') {
